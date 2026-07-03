8d40e8aa8925816ce118293565b5f24d6f04829f
brian1234570 <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>
2026-07-03 13:27:47 +0200

    Add 8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
-"""Module that prints a string in uppercase."""
-
-
 def uppercase(str):
-    """Print a string in uppercase followed by a new line.
-
-    Args:
-        str (str): the string to print in uppercase.
-    """
-    ustr = ""
     for c in str:
-        if ord(c) >= 97 and ord(c) <= 122:
-            ustr += chr(ord(c) - 32)
-        else:
-            ustr += c
-    print("{}".format(ustr))
+        n = ord(c)
+        if 97 <= n <= 122:
+            n -= 32
+        print("{:c}".format(n), end="")
+    print("")
